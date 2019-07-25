acff519706fa259d0edb324ec84915886558c8c6
Patrick Pircher <patrick.pircher@ibm.com>
2019-07-25 19:08:25 +0200

    add stories

--- src/ui/components/button/component.js
+++ src/ui/components/button/component.js
@@ -1,14 +1,17 @@
 import Component from '@ember/component';
 import BxClassNames from 'carbon/src/mixins/bx-class-names';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default Component.extend(BxClassNames, {
-  tagName: '',
-  classPrefix: 'bx--btn--',
-  confirmText: null,
-  dialogManager: service('carbon::dialog-manager'),
+class CarbonButton extends Component.extend(BxClassNames) {
+  tagName = '';
+  classPrefix = 'bx--btn--'
+  confirmText = null;
+
+  @service('carbon::dialog-manager') dialogManager;
 
   init(...args) {
+    super.init(...args);
     this.classMappings = [
       'primary:primary',
       'secondary:secondary',
@@ -16,37 +19,37 @@ export default Component.extend(BxClassNames, {
       'ghost:ghost',
       'small:sm'
     ];
-    this._super(...args);
-  },
-
-  actions: {
-    onClick(...args) {
-      const run = () => {
-        const action = this.get('onClick');
-        if (action) {
-          const ret = action(...args);
-          if (ret && ret.finally) {
-            this.set('loading', true);
-            this.set('disabled', true);
-            ret.finally(() => {
-              this.set('loading', false);
-              this.set('disabled', false);
-            });
-          }
-        }
-      };
-      if (this.danger) {
-        this.dialogManager.open('confirm', {
-          type: 'danger',
-          header: 'Danger',
-          body: this.confirmText || 'Confirm this operation',
-          onAccept: run
-        });
-      } else {
-        run();
-      }
-      // Prevent bubbling, if specified. If undefined, the event will bubble.
-      return this.get('bubbles');
-    }
   }
-});
+
+  @action
+  onClick(...args) {
+    const run = () => {
+      const action = this.get('onClick');
+      if (action) {
+        const ret = action(...args);
+        if (ret && ret.finally) {
+          this.set('loading', true);
+          this.set('disabled', true);
+          ret.finally(() => {
+            this.set('loading', false);
+            this.set('disabled', false);
+          });
+        }
+      }
+    };
+    if (this.danger) {
+      this.dialogManager.open('confirm', {
+        type: 'danger',
+        header: 'Danger',
+        body: this.confirmText || 'Confirm this operation',
+        onAccept: run
+      });
+    } else {
+      run();
+    }
+    // Prevent bubbling, if specified. If undefined, the event will bubble.
+    return this.get('bubbles');
+  }
+}
+
+export default CarbonButton;
---
+++ stories/index.js
@@ -0,0 +1,25 @@
+import hbs from 'htmlbars-inline-precompile';
+import { storiesOf } from '@storybook/ember';
+
+storiesOf('Demo', module)
+  .add('heading', () => hbs`<h1>Hello World</h1>`)
+  .add('button', () => {
+    return {
+      template: hbs`<button {{action onClick}}>
+        Hello Button
+      </button>`,
+      context: {
+        onClick: (e) => console.log(e)
+      }
+    }
+  })
+  .add('component', () => {
+    return {
+      template: hbs`{{foo-bar
+        click=onClick
+      }}`,
+      context: {
+        onClick: (e) => console.log(e)
+      }
+    }
+  });
